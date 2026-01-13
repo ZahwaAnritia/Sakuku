9ddd3c10392a502ce6cebf66c1b6afc13c89e62c
ZahwaAnritia <anritia15@gmail.com>
2026-01-13 21:26:08 +0700

    setting gradle

---
+++ app/src/main/java/com/example/comzahwasakuku/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.comzahwasakuku.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// --- WARNA TEMA BARU (CYAN & ORANGE) ---
+val CyanPrimary = Color(0xFF00BCD4)      // Warna Utama (Biru Laut)
+val CyanDark = Color(0xFF0097A7)         // Versi Gelap
+val CyanLight = Color(0xFFB2EBF2)        // Versi Terang
+
+val OrangeAccent = Color(0xFFFFF3E0)     // Untuk Card Indomie
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF1D1B20)
+
+// Warna Ungu (Hapus saja kalau tidak dipakai, atau biarkan kalau ragu)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
