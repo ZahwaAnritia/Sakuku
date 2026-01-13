770c43c9bab8c7d45edbe92eba04de66f487b603
ZahwaAnritia <anritia15@gmail.com>
2026-01-13 21:28:19 +0700

    add file category dao

---
+++ app/src/main/java/com/example/comzahwasakuku/data/local/dao/CategoryDao.kt
@@ -0,0 +1,32 @@
+package com.example.comzahwasakuku.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.comzahwasakuku.data.local.entity.CategoryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryDao {
+    @Query("SELECT * FROM categories WHERE user_id = :userId OR user_id = 0 ORDER BY name ASC")
+    fun getAllCategories(userId: Int): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategory(category: CategoryEntity)
+
+    // --- TAMBAHKAN INI AGAR REPOSITORY TIDAK ERROR ---
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(categories: List<CategoryEntity>)
+    // -------------------------------------------------
+
+    @Delete
+    suspend fun deleteCategory(category: CategoryEntity)
+
+    @Query("SELECT COUNT(*) FROM categories")
+    suspend fun getCount(): Int
+
+    @Query("UPDATE categories SET name = :newName WHERE name = :oldName AND user_id = 0")
+    suspend fun renameSystemCategory(oldName: String, newName: String)
+}
