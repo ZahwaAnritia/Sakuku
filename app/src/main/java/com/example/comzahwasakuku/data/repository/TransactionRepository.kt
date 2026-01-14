53469c56c70504aae8b9699e100da5f855125362
ZahwaAnritia <anritia15@gmail.com>
2026-01-14 22:21:15 +0700

    add file transaction repository

---
+++ app/src/main/java/com/example/comzahwasakuku/data/repository/TransactionRepository.kt
@@ -0,0 +1,21 @@
+package com.example.comzahwasakuku.data.repository
+
+import com.example.comzahwasakuku.data.local.dao.TransactionDao
+import com.example.comzahwasakuku.data.local.entity.TransactionEntity
+import kotlinx.coroutines.flow.Flow
+
+class TransactionRepository(private val transactionDao: TransactionDao) {
+
+    suspend fun insertTransaction(transaction: TransactionEntity) {
+        transactionDao.insertTransaction(transaction)
+    }
+
+    // --- REVISI: Ganti Email (String) jadi User ID (Int) ---
+    fun getAllTransactions(userId: Int): Flow<List<TransactionEntity>> {
+        return transactionDao.getAllTransactions(userId)
+    }
+
+    suspend fun deleteTransaction(transaction: TransactionEntity) {
+        transactionDao.deleteTransaction(transaction)
+    }
+}
