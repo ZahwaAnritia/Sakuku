f891ac4d7149c30a03e863ce25c9df7f98384e7d
ZahwaAnritia <anritia15@gmail.com>
2026-01-15 17:39:55 +0700

    menambahkan TransactionViewModel untuk pengelolaan transaksi pengguna

---
+++ app/src/main/java/com/example/comzahwasakuku/ui/viewmodel/TransactionViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.comzahwasakuku.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.comzahwasakuku.data.local.entity.TransactionEntity
+import com.example.comzahwasakuku.data.repository.TransactionRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class TransactionViewModel(
+    private val repository: TransactionRepository
+) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    // Fungsi Add terima userId Int
+    fun addTransaction(
+        userId: Int, // <--- GANTI JADI INT
+        amount: Double,
+        category: String,
+        note: String,
+        type: String,
+        date: Long,
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+
+        if (amount <= 0.0) {
+            onError("Nominal transaksi tidak boleh 0 atau kosong!")
+            return // Berhenti di sini, jangan lanjut simpan ke database
+        }
+        viewModelScope.launch {
+            _isLoading.value = true
+            try {
+                val transaction = TransactionEntity(
+                    userId = userId, // <--- SIMPAN ID
+                    amount = amount,
+                    category = category,
+                    note = note,
+                    type = type,
+                    date = date
+                )
+                repository.insertTransaction(transaction)
+                _isLoading.value = false
+                onSuccess()
+            } catch (e: Exception) {
+                _isLoading.value = false
+                onError(e.message ?: "Gagal menyimpan transaksi")
+            }
+        }
+    }
+
+    fun deleteTransaction(transaction: TransactionEntity) {
+        viewModelScope.launch {
+            repository.deleteTransaction(transaction)
+        }
+    }
+}
