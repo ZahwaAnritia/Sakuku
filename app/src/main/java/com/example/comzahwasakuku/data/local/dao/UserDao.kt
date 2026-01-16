b5142513d684cf486db4ab5e19eee4e2d7ac4f66
ZahwaAnritia <anritia15@gmail.com>
2026-01-16 18:45:02 +0700

    Update last_login_date user berdasarkan email

--- app/src/main/java/com/example/comzahwasakuku/data/local/dao/UserDao.kt
+++ app/src/main/java/com/example/comzahwasakuku/data/local/dao/UserDao.kt
@@ -13,4 +13,7 @@ interface UserDao {
     suspend fun loginLocal(email: String, password: String): UserEntity?
     @Query("UPDATE users SET target_limit = :limit, indomie_price = :price WHERE email = :email")
     suspend fun updateUserProfile(email: String, limit: Double, price: Double)
+
+    @Query("UPDATE users SET last_login_date = :date WHERE email = :email")
+    suspend fun updateLastLogin(email: String, date: Long)
 }
